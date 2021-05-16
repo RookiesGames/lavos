940ce4807beaa88db81c588399bad74b54d5195b
Duarte Roso <duarte.roso@posteo.eu>
2021-05-16 12:03:23 +0200

    LoadSceneAsync

--- core/scene/SceneManager.cs
+++ core/scene/SceneManager.cs
@@ -1,5 +1,7 @@
 using Godot;
 using Vortico.Core.Debug;
+using System;
+using System.Threading.Tasks;
 
 namespace Vortico.Core.Scene
 {
@@ -24,18 +26,19 @@ namespace Vortico.Core.Scene
         #endregion
 
         #region LoadScene
-        public static void LoadScene(PackedScene scene)
+        public static void ChangeScene(PackedScene scene)
         {
-            LoadScene(scene.ResourcePath);
+            EmptyScene();
+            AddPackedSceneToParent(scene, _sceneNode);
         }
 
-        public static void LoadScene(string path)
+        public static void ChangeScene(string path)
         {
-            CleanupScene();
-            AddScene(path, _sceneNode);
+            EmptyScene();
+            AddScenePathToParent(path, _sceneNode);
         }
 
-        private static void CleanupScene()
+        private static void EmptyScene()
         {
             var children = _sceneNode.GetChildren();
             foreach (Node child in children)
@@ -46,17 +49,47 @@ namespace Vortico.Core.Scene
         }
         #endregion
 
-
-        #region AddScene
-        public static void AddScene(PackedScene scene, Node parent)
-        {
-            AddScene(scene.ResourcePath, parent);
-        }
-
-        public static void AddScene(string path, Node parent)
+        #region LoadScene
+        public static PackedScene LoadScene(string path)
         {
             var packedScene = GD.Load<PackedScene>(path);
-            var node = packedScene.Instance();
+            return packedScene;
+        }
+
+        public static void LoadScene(string path, Action<PackedScene> completeAction)
+        {
+            LoadSceneAsync(path, completeAction);
+        }
+
+        private static async void LoadSceneAsync(string path, Action<PackedScene> completeAction)
+        {
+            var task = Task.Factory.StartNew<PackedScene>(() => LoadScene(path));
+            await task;
+            completeAction?.Invoke(task.Result);
+        }
+        #endregion
+
+        #region AddScene
+        public static void AddPackedSceneToParent(PackedScene scene, Node parent)
+        {
+            var node = InstantiateScene(scene);
+            AddNode(node, parent);
+        }
+
+        public static void AddScenePathToParent(string path, Node parent)
+        {
+            var packedScene = LoadScene(path);
+            var node = InstantiateScene(packedScene);
+            AddNode(node, parent);
+        }
+
+        private static Node InstantiateScene(PackedScene scene)
+        {
+            return scene.Instance();
+        }
+
+        private static void AddNode(Node node, Node parent)
+        {
             parent.AddChild(node, true);
         }
         #endregion
