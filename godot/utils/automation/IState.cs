eb354fdbc911e42ef9831151e15a2df356d6aeb3
Duarte Roso <duarteroso@users.noreply.github.com>
2022-04-03 20:46:03 +0200

    Add tag to state

--- godot/utils/automation/IState.cs
+++ godot/utils/automation/IState.cs
@@ -4,6 +4,7 @@ namespace Lavos.Utils.Automation
 {
     public interface IState
     {
+        string Tag { get; }
         Action Enter { get; set; }
         Action<float> Process { get; set; }
         Action Exit { get; set; }
--- godot/utils/automation/State.cs
+++ godot/utils/automation/State.cs
@@ -4,6 +4,7 @@ namespace Lavos.Utils.Automation
 {
     public sealed class State : IState
     {
+        public string Tag { get; set; }
         public Action Enter { get; set; }
         public Action<float> Process { get; set; }
         public Action Exit { get; set; }
