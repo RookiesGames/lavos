56394aefaa1e92d1a9a1675ecf5b37778b905dc2
Duarte Roso <duarteroso@users.noreply.github.com>
2023-04-29 09:34:20 +0200

    Simplify LazyRef

--- godot/utils/lazy/LazyRef.cs
+++ godot/utils/lazy/LazyRef.cs
@@ -4,12 +4,6 @@ namespace Lavos.Utils.Lazy;
 
 public struct LazyRef<T> where T : IService
 {
-    LazyRef(T instance = default)
-    {
-        _instance = instance;
-    }
-
     T _instance;
-
-    public T Ref => _instance != null ? _instance : (_instance = ServiceLocator.Locate<T>());
+    public T Ref => _instance ??= ServiceLocator.Locate<T>();
 }
