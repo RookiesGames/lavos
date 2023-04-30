60462d150e94d7ba581cadc062988e3801b63b21
Duarte Roso <duarteroso@users.noreply.github.com>
2023-04-30 13:37:54 +0200

    Add local and global vector helpers

--- godot/utils/extensions/Node3DExtensions.cs
+++ godot/utils/extensions/Node3DExtensions.cs
@@ -4,10 +4,11 @@ namespace Lavos.Utils.Extensions;
 
 public static class Node3DExtensions
 {
-    public static Vector3 GetForwardVector(this Node3D node)
-    {
-        return -1 * node.Basis.Z;
-    }
+    public static Vector3 GetLocalUp(this Node3D node) => node.Basis.Y;
+    public static Vector3 GetLocalForward(this Node3D node) => -1 * node.Basis.Z;
+
+    public static Vector3 GetGlobalUp(this Node3D node) => node.GlobalTransform.Basis.Y;
+    public static Vector3 GetGlobalForward(this Node3D node) => -1 * node.GlobalTransform.Basis.Z;
 
     public static void LookAtTarget(this Node3D node, Vector3 target)
     {
