5824448b577635582888be39dead56ab0383b41d
Duarte Roso <duarteroso@users.noreply.github.com>
2023-05-30 18:00:53 +0200

    Set default of LazyPin instance to null

--- godot/utils/lazy/LazyPin.cs
+++ godot/utils/lazy/LazyPin.cs
@@ -4,6 +4,6 @@ namespace Lavos.Utils.Lazy;
 
 public sealed class LazyPin<T> where T : Node
 {
-    T _instance = default;
+    T _instance = null;
     public T Pin => _instance ??= NodeTree.GetPinnedNodeByType<T>();
 }
