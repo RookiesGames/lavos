94f36fbab9d977f0483b568f335a1b78dc808252
Duarte Roso <duarteroso@users.noreply.github.com>
2023-05-30 10:29:29 +0200

    Update FadePanel.cs

--- godot/ui/fade/FadePanel.cs
+++ godot/ui/fade/FadePanel.cs
@@ -1,12 +1,13 @@
 using Godot;
 using Lavos.Utils.Automation;
+using System;
 using System.Threading.Tasks;
 
 namespace Lavos.UI;
 
 public sealed partial class FadePanel : ColorRect
 {
-    [Export] bool Faded = false;
+    [Export] bool Faded;
     [Export] double FadeInDuration = 1;
     [Export] double FadeOutDuration = 1;
 
@@ -17,11 +18,22 @@ public sealed partial class FadePanel : ColorRect
     bool IsFadingIn => _stateMachine.CurrentState == _fadeInState;
     bool IsFadingOut => _stateMachine.CurrentState == _fadeOutState;
 
+    public event Action FadedIn;
+    public event Action FadedOut;
+
     public override void _Ready()
     {
         this.SetAlpha(Faded ? 1f : 0f);
+        MouseFilter = Faded ? MouseFilterEnum.Stop : MouseFilterEnum.Ignore;
         _fadeInState = new FadeInState(this, FadeInDuration);
         _fadeOutState = new FadeOutState(this, FadeOutDuration);
+        //
+        NodeTree.PinNodeByType<FadePanel>(this);
+    }
+
+    public override void _ExitTree()
+    {
+        NodeTree.UnpinNodeByType<FadePanel>();
     }
 
     public override void _Process(double delta)
@@ -32,13 +44,15 @@ public sealed partial class FadePanel : ColorRect
     public Task FadeIn()
     {
         _stateMachine.ChangeState(_fadeInState);
+        await Task.w
         return Task.Run(async () =>
         {
-            System.TimeSpan timeSpan = System.TimeSpan.FromMilliseconds(60);
+            TimeSpan timeSpan = TimeSpan.FromMilliseconds(60);
             while (IsFadingIn)
             {
                 await Task.Delay(timeSpan);
             }
+            _stateMachine.ChangeState(null);
             return Task.CompletedTask;
         });
     }
@@ -48,11 +62,12 @@ public sealed partial class FadePanel : ColorRect
         _stateMachine.ChangeState(_fadeOutState);
         return Task.Run(async () =>
         {
-            System.TimeSpan timeSpan = System.TimeSpan.FromMilliseconds(60);
+            TimeSpan timeSpan = TimeSpan.FromMilliseconds(60);
             while (IsFadingOut)
             {
                 await Task.Delay(timeSpan);
             }
+            _stateMachine.ChangeState(null);
             return Task.CompletedTask;
         });
     }
