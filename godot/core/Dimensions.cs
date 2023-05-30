bfa5270dc9b7e2dbda0c5ac8d194284a961afd2a
Duarte Roso <duarteroso@users.noreply.github.com>
2023-05-30 18:00:57 +0200

    Create Dimensions.cs

---
+++ godot/core/Dimensions.cs
@@ -0,0 +1,13 @@
+namespace Lavos.Core;
+
+public sealed class Dimensions
+{
+    public readonly int Width;
+    public readonly int Height;
+
+    public Dimensions(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+}
