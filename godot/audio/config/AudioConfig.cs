4897eb34c37f51dffa4c73a7c9798e254894a110
Duarte Roso <duarteroso@users.noreply.github.com>
2023-05-30 14:59:44 +0200

    Add audio nodes in initialize step

--- godot/audio/config/AudioConfig.cs
+++ godot/audio/config/AudioConfig.cs
@@ -6,13 +6,12 @@ namespace Lavos.Audio;
 
 sealed partial class AudioConfig : Config
 {
-    public override void Configure(IDependencyBinder binder)
+    public override void Configure(IDependencyBinder binder) { }
+    public override void Initialize(IDependencyResolver resolver)
     {
         var audio = OmniNode.Instance.AddNode<Node>("Audio");
         audio.AddNode<MasterAudio>();
         audio.AddNode<MusicManager>();
         audio.AddNode<SoundManager>();
     }
-
-    public override void Initialize(IDependencyResolver resolver) { }
 }
