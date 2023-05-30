db5d282a0867ac8a3f44550e8d3d818650230f85
Duarte Roso <duarteroso@users.noreply.github.com>
2023-05-30 10:29:15 +0200

    Create SceneTransition.cs

---
+++ godot/scene/SceneTransition.cs
@@ -0,0 +1,23 @@
+using Godot;
+
+namespace Lavos.Scene;
+
+public sealed partial class SceneTransition : Node
+{
+    [Export] PackedScene _scene;
+
+    public override void _EnterTree()
+    {
+        NodeTree.PinNodeByType<SceneTransition>(this);
+    }
+
+    public override void _ExitTree()
+    {
+        NodeTree.UnpinNodeByType<SceneTransition>();
+    }
+
+    public void Transition()
+    {
+        SceneManager.ChangeScene(_scene);
+    }
+}
