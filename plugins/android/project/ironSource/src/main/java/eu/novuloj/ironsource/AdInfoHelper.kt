bf23ccfdbe099d48e8ef2e1cb9998cff3c3db323
Duarte Roso <duarteroso@users.noreply.github.com>
2023-07-17 11:58:47 +0200

    Add Android ironSource module

---
+++ plugins/android/project/ironSource/src/main/java/eu/novuloj/ironsource/AdInfoHelper.kt
@@ -0,0 +1,30 @@
+package eu.novuloj.ironsource
+
+import com.ironsource.mediationsdk.adunit.adapter.utility.AdInfo
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import kotlinx.serialization.json.putJsonObject
+
+class AdInfoHelper {
+    companion object {
+        fun toJson(adInfo: AdInfo): JsonObject {
+            return buildJsonObject {
+                putJsonObject("ad_info") {
+                    put("auction_id", adInfo.auctionId)
+                    put("ad_unit", adInfo.adUnit)
+                    put("country", adInfo.country)
+                    put("ab", adInfo.ab)
+                    put("segment_name", adInfo.segmentName)
+                    put("ad_network", adInfo.adNetwork)
+                    put("instance_name", adInfo.instanceName)
+                    put("instance_id", adInfo.instanceId)
+                    put("precision", adInfo.precision)
+                    put("revenue", adInfo.revenue)
+                    put("lifetime_revenue", adInfo.lifetimeRevenue)
+                    put("encrypted_cpm", adInfo.encryptedCPM)
+                }
+            }
+        }
+    }
+}
---
+++ plugins/android/project/ironSource/src/main/java/eu/novuloj/ironsource/interstitials/InterstitialPlacementHelper.kt
@@ -0,0 +1,20 @@
+package eu.novuloj.ironsource.interstitials
+
+import com.ironsource.mediationsdk.model.InterstitialPlacement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import kotlinx.serialization.json.putJsonObject
+
+class InterstitialPlacementHelper {
+    companion object {
+        fun toJson(placement: InterstitialPlacement): JsonObject {
+            return buildJsonObject {
+                putJsonObject("placement") {
+                    put("id", placement.placementId)
+                    put("name", placement.placementName)
+                }
+            }
+        }
+    }
+}
