e4221a75a0acd149590bcf8a09fe20cdf375742d
Duarte Roso <duarteroso@users.noreply.github.com>
2023-07-23 18:24:31 +0200

    Camera changes

--- addons/lavos/nodes/3d/camera/BaseCamera.cs
+++ addons/lavos/nodes/3d/camera/BaseCamera.cs
@@ -6,4 +6,13 @@ public abstract partial class BaseCamera : Camera3D
 {
     [Export] CameraConfig _config;
     protected CameraConfig Config => _config;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        // Set camera
+        Position = new Vector3(0f, Config.Height, Config.Distance);
+        LookAt(Vector3.Zero, this.GetLocalUp());
+    }
 }
--- addons/lavos/nodes/3d/camera/TrackingCamera.cs
+++ addons/lavos/nodes/3d/camera/TrackingCamera.cs
@@ -5,5 +5,12 @@ namespace Lavos.Nodes.Camera;
 [GlobalClass]
 public partial class TrackingCamera : BaseCamera
 {
+    [Export] public Node3D Target;
 
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+
+        // Track target
+    }
 }
