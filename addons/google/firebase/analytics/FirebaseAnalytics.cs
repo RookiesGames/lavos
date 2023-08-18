484be338b93efb18b3def6c1a9b2126e1ce75590
Duarte Roso <duarteroso@users.noreply.github.com>
2023-08-18 17:43:02 +0200

    Add firebase addons

--- addons/google/firebase/analytics/FirebaseAnalytics.cs
+++ addons/google/firebase/analytics/FirebaseAnalytics.cs
@@ -1,6 +1,20 @@
+#if TOOLS
 using Godot;
-using System;
 
+namespace Lavos.Addons;
+
+[Tool]
 public partial class FirebaseAnalytics : EditorExportPlugin
 {
+    public override string _GetName() => nameof(FirebaseAnalytics);
+    public override bool _SupportsPlatform(EditorExportPlatform platform)
+    {
+        return platform is EditorExportPlatformAndroid || platform is EditorExportPlatformIos;
+    }
+    public override string[] _GetAndroidDependencies(EditorExportPlatform platform, bool debug)
+    {
+        Assert.IsTrue(platform is EditorExportPlatformAndroid, "Wrong platform used");
+        return new string[] { "res://addons/rookies/google/firebase/analytics/lib/android/FirebaseAnalytics.aar" };
+    }
 }
+#endif
