2fce8245ed06ac524807c42a1c93fd0c76eab9c6
Duarte Roso <duarteroso@users.noreply.github.com>
2023-10-05 08:11:00 +0200

    Update GoogleAddons.cs

--- addons/google/GoogleAddons.cs
+++ addons/google/GoogleAddons.cs
@@ -1,10 +1,12 @@
-#if TOOLS
 
 namespace Lavos.Addons.Google;
 
 public static class GoogleAddons
 {
-    public static string Root => "res://addons/rookies/google";
-}
+    public const string Root = "res://addons/rookies/google";
 
-#endif
+    public const string FirebaseAnalyticsPluginName = "FirebaseAnalytics";
+    public const string FirebaseCrashlyticsPluginName = "FirebaseCrashlytics";
+
+    public const string AdMobPluginName = "AdMob";
+}
