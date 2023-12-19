14819c6bef49c728d5aa3921402001095c486b69
Duarte Roso <duarteroso@users.noreply.github.com>
2023-12-19 13:14:08 +0100

    Godot plugin setup

---
+++ addons/google/billing/GoogleBillingEditorExportPlugin.cs
@@ -0,0 +1,34 @@
+#if TOOLS
+using Godot;
+
+namespace Lavos.Plugins.Google.Billing;
+
+[Tool]
+public sealed partial class GoogleBillingExportEditorPlugin : EditorExportPlugin
+{
+    public override string _GetName() => GooglePlugins.PluginNames.GoogleBilling;
+
+    public override bool _SupportsPlatform(EditorExportPlatform platform)
+    {
+        return platform is EditorExportPlatformAndroid;
+    }
+
+    public override string [] _GetAndroidLibraries(EditorExportPlatform platform, bool debug)
+    {
+        var aar = $"{_GetName()}.{(debug ? "debug" : "release")}.aar";
+        return new string[]
+        {
+            $"{GooglePlugins.Root}/billing/.bin/android/{aar}"
+        };
+    }
+
+    public override string[] _GetAndroidDependencies(EditorExportPlatform platform, bool debug)
+    {
+        return new[] 
+        {
+            "com.android.billingclient:billing-ktx:6.0.1",
+            "org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.4.1"
+        };
+    }
+}
+#endif
--- addons/google/billing/GoogleBillingEditorPlugin.cs
+++ addons/google/billing/GoogleBillingEditorPlugin.cs
@@ -6,5 +6,18 @@ namespace Lavos.Plugins.Google.Billing;
 [Tool]
 public sealed partial class GoogleBillingEditorPlugin : EditorPlugin
 {
+    GoogleBillingExportEditorPlugin Plugin;
+
+    public override void _EnterTree()
+    {
+        Plugin = new GoogleBillingExportEditorPlugin();
+        AddExportPlugin(Plugin);
+    }
+
+    public override void _ExitTree()
+    {
+        RemoveExportPlugin(Plugin);
+        Plugin = null;
+    }
 }
 #endif
