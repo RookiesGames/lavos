d6b38d75fd7d707b97fee43b019cbbde3bcfe297
Duarte Roso <duarteroso@users.noreply.github.com>
2023-12-20 12:52:28 +0100

    Fix json converter not adding parts

--- plugins/android/project/GoogleBilling/src/main/java/eu/rookies/google/billing/ProductHelper.kt
+++ plugins/android/project/GoogleBilling/src/main/java/eu/rookies/google/billing/ProductHelper.kt
@@ -12,69 +12,53 @@ class ProductHelper {
     companion object {
         fun toJson(product: ProductDetails): JsonObject {
             return buildJsonObject {
-                putJsonObject("product") {
-                    put("id", product.productId)
-                    put("name", product.name)
-                    put("description", product.description)
-                    put("type", product.productType)
-                    if (product.oneTimePurchaseOfferDetails != null) {
-                        toJson(product.oneTimePurchaseOfferDetails!!)
-                    }
-                    if (product.subscriptionOfferDetails != null) {
-                        toJson(product.subscriptionOfferDetails!!)
-                    }
-                }
-            }
-        }
-
-        private fun toJson(details: ProductDetails.OneTimePurchaseOfferDetails): JsonObject {
-            return buildJsonObject {
-                putJsonObject("price") {
-                    put("formatted", details.formattedPrice)
-                    put("currency", details.priceCurrencyCode)
-                    put("amount", details.priceAmountMicros)
-                }
-            }
-        }
-
-        private fun toJson(details: List<ProductDetails.SubscriptionOfferDetails>): JsonObject {
-            return buildJsonObject {
-                putJsonArray("subscriptions") {
-                    for (detail in details) {
-                        add(toJson(detail))
+                put("id", product.productId)
+                put("name", product.name)
+                put("description", product.description)
+                put("type", product.productType)
+                if (product.oneTimePurchaseOfferDetails != null) {
+                    val details = product.oneTimePurchaseOfferDetails!!
+                    putJsonObject("price") {
+                        put("formatted", details.formattedPrice)
+                        put("currency", details.priceCurrencyCode)
+                        put("amount", details.priceAmountMicros)
                     }
                 }
-            }
-        }
-
-        private fun toJson(details: ProductDetails.SubscriptionOfferDetails): JsonObject {
-            return buildJsonObject {
-                putJsonObject("subscription") {
-                    put("plan_id", details.basePlanId)
-                    putJsonObject("offer") {
-                        put("id", details.offerId)
-                        put("token", details.offerToken)
-                        putJsonArray("tags") {
-                            for (tag in details.offerTags) {
-                                add(tag)
-                            }
-                        }
-                    }
-                    putJsonArray("phases") {
-                        for (phase in details.pricingPhases.pricingPhaseList) {
-                            val entry = buildJsonObject {
-                                put("mode", phase.recurrenceMode)
-                                putJsonObject("billing") {
-                                    put("period", phase.billingPeriod)
-                                    put("cycle", phase.billingCycleCount)
+                if (product.subscriptionOfferDetails != null) {
+                    val details = product.subscriptionOfferDetails!!
+                    putJsonArray("subscriptionsOfferDetails") {
+                        for (detail in details) {
+                            add(
+                                buildJsonObject {
+                                    put("id", detail.basePlanId)
+                                    putJsonObject("offer") {
+                                        put("id", detail.offerId)
+                                        put("token", detail.offerToken)
+                                        putJsonArray("tags") {
+                                            for (tag in detail.offerTags) {
+                                                add(tag)
+                                            }
+                                        }
+                                    }
+                                    putJsonArray("phases") {
+                                        for (phase in detail.pricingPhases.pricingPhaseList) {
+                                            val entry = buildJsonObject {
+                                                put("mode", phase.recurrenceMode)
+                                                putJsonObject("billing") {
+                                                    put("period", phase.billingPeriod)
+                                                    put("cycle", phase.billingCycleCount)
+                                                }
+                                                putJsonObject("price") {
+                                                    put("formatted", phase.formattedPrice)
+                                                    put("currency", phase.priceCurrencyCode)
+                                                    put("amount", phase.priceAmountMicros)
+                                                }
+                                            }
+                                            add(entry)
+                                        }
+                                    }
                                 }
-                                putJsonObject("price") {
-                                    put("formatted", phase.formattedPrice)
-                                    put("currency", phase.priceCurrencyCode)
-                                    put("amount", phase.priceAmountMicros)
-                                }
-                            }
-                            add(entry)
+                            )
                         }
                     }
                 }
