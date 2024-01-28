36f17e8cc1a2664811cc50299dfaf97f2e6a128a
Duarte Roso <duarteroso@users.noreply.github.com>
2024-01-28 20:47:56 +0100

    Update LazyNode.cs

--- addons/lavos/core/lazy/LazyNode.cs
+++ addons/lavos/core/lazy/LazyNode.cs
@@ -1,18 +1,10 @@
 using Godot;
-using Lavos.Scene;
+using Lavos.Nodes;
 
 namespace Lavos.Core;
 
 public struct LazyNode<T> where T : Node
 {
-    public LazyNode(string name, Node parent = null) 
-    {
-        _parent = parent ?? NodeTree.Instance;
-        _name = name;
-    }
-
-    Node _parent;
-    string _name;
     T _instance;
-    public T Node => _instance ??= _parent.GetNodeInChildrenByName<T>(_name);
+    public T Node => _instance ??= OmniNode.Instance.GetNodeInChildren<T>();
 }
