bd7da1ff613f5bb9553b013f397043b99d9da4ff
Duarte Roso <duarteroso@users.noreply.github.com>
2024-01-29 09:47:20 +0100

    Cache serializer options

--- addons/lavos/utils/JsonHelper.cs
+++ addons/lavos/utils/JsonHelper.cs
@@ -4,10 +4,11 @@ namespace Lavos.Utils;
 
 public static class JsonHelper
 {
+    static readonly JsonSerializerOptions _options = new() { PropertyNameCaseInsensitive = true };
+
     public static T Deserialize<T>(string json)
     {
-        var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
-        var data = JsonSerializer.Deserialize<T>(json, options);
+        var data = JsonSerializer.Deserialize<T>(json, _options);
         return data;
     }
 }
