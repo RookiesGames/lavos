3d094dc2f16def00bfabb148e645e92c8eda5e95
Duarte Roso <duarteroso@users.noreply.github.com>
2024-01-31 22:44:22 +0100

    Make Range use doubles

--- addons/lavos/math/Range.cs
+++ addons/lavos/math/Range.cs
@@ -4,24 +4,26 @@ namespace Lavos.Math;
 
 public sealed class Range
 {
-    float _value;
-    public float Value
+    double _value;
+    public double Value
     {
         get => _value;
         set => _value = Mathf.Clamp(value, Min, Max);
     }
-    public float Min { get; }
-    public float Max { get; }
+    public double Min { get; }
+    public bool IsMin => _value == Min;
+    public double Max { get; }
+    public bool IsMax => _value == Max;
 
-    public Range(float value, float min, float max)
+    public Range(double value, double min, double max)
     {
         _value = value;
         Min = min;
         Max = max;
     }
 
-    public void Add(float value) => _value = Mathf.Clamp(_value + value, Min, Max);
-    public void Substract(float value) => _value = Mathf.Clamp(_value - value, Min, Max);
-    public void Multiply(float value) => _value = Mathf.Clamp(_value * value, Min, Max);
-    public void Divice(float value) => _value = Mathf.Clamp(_value / value, Min, Max);
+    public void Add(double value) => _value = Mathf.Clamp(_value + value, Min, Max);
+    public void Substract(double value) => _value = Mathf.Clamp(_value - value, Min, Max);
+    public void Multiply(double value) => _value = Mathf.Clamp(_value * value, Min, Max);
+    public void Divice(double value) => _value = Mathf.Clamp(_value / value, Min, Max);
 }
