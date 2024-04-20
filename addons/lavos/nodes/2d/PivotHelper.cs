6d1c83a5d56d9e9d9a75725998ffedd8817c0668
Duarte Roso <duarteroso@users.noreply.github.com>
2024-04-20 14:04:40 +0200

    Create PivotHelper.cs

---
+++ addons/lavos/nodes/2d/PivotHelper.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+[Tool]
+public partial class PivotHelper : Control
+{
+	[Export]
+	Vector2 _range;
+
+	public override void _EnterTree()
+	{
+		PivotOffset = new Vector2(Size.X * _range.X, Size.Y * _range.Y);
+	}
+
+	public override void _Process(double delta)
+	{
+		PivotOffset = new Vector2(Size.X * _range.X, Size.Y * _range.Y);
+	}
+}
