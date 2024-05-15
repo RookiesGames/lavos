d86712f196d65d6dda5be13ad21ea099c28427e5
Duarte Roso <duarteroso@users.noreply.github.com>
2024-05-15 13:04:49 +0200

    Add parent parameter to LazyNode

--- addons/lavos/core/lazy/LazyNode.cs
+++ addons/lavos/core/lazy/LazyNode.cs
@@ -5,6 +5,10 @@ namespace Lavos.Core;
 
 public struct LazyNode<T> where T : Node
 {
+    public LazyNode() : this(OmniNode.Instance) { }
+    public LazyNode(Node parent) { _parent = parent; }
+
+    Node _parent = null;
     T _instance;
-    public T Node => _instance ??= OmniNode.Instance.GetNodeInChildrenByType<T>();
+    public T Node => _instance ??= _parent.GetNodeInChildrenByType<T>();
 }
