aa5ae5974efbbbffc3a9fd2c0041f1cf95760353
Duarte Roso <duarteroso@users.noreply.github.com>
2024-05-31 19:49:40 +0200

    Update FadePanelConfig.cs

--- addons/lavos/ui/fade/FadePanelConfig.cs
+++ addons/lavos/ui/fade/FadePanelConfig.cs
@@ -5,9 +5,9 @@ namespace Lavos.UI;
 [GlobalClass]
 public sealed partial class FadePanelConfig : Resource
 {
-    [Export] double _fadeInDuration = 1;
-    public double FadeInDuration => _fadeInDuration;
+    [Export] uint _fadeInDuration = 1;
+    public uint FadeInDuration => _fadeInDuration;
 
-    [Export] double _fadeOutDuration = 1;
-    public double FadeOutDuration => _fadeOutDuration;
+    [Export] uint _fadeOutDuration = 1;
+    public uint FadeOutDuration => _fadeOutDuration;
 }
