38e5de8501c395fcc55f4c0de3901fbe26c576c8
Duarte Roso <duarteroso@users.noreply.github.com>
2024-07-10 10:12:30 +0200

    Remove unnecessary collection

--- addons/lavos/input/controllers/keyboard/config/IKeyboardInputConfig.cs
+++ addons/lavos/input/controllers/keyboard/config/IKeyboardInputConfig.cs
@@ -1,10 +1,8 @@
 using Godot;
-using System.Collections.Generic;
 
 namespace Lavos.Input;
 
 public interface IKeyboardInputConfig : IInputConfig
 {
-    IReadOnlyCollection<Godot.Key> Keys { get; }
-    InputAction GetAction(Godot.Key key);
+    InputAction GetAction(Key key);
 }
