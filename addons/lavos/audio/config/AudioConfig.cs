c90aa59644846a7b0190d27f307b2c0ca311f42e
Duarte Roso <duarteroso@users.noreply.github.com>
2024-07-11 10:15:34 +0200

    Make AudioConfig a DataSaverConfig

--- addons/lavos/audio/config/AudioConfig.cs
+++ addons/lavos/audio/config/AudioConfig.cs
@@ -1,14 +1,16 @@
 using Godot;
 using Lavos.Dependency;
 using Lavos.Nodes;
+using Lavos.Services.Data;
 
 namespace Lavos.Audio;
 
-sealed partial class AudioConfig : Config
+sealed partial class AudioConfig : DataSaverConfig
 {
-    public override void Configure(IDependencyBinder binder) { }
     public override void Initialize(IDependencyResolver resolver)
     {
+        base.Initialize(resolver);
+        //
         var audio = OmniNode.Instance.AddNode<Node>("Audio");
         audio.AddNode<MasterAudio>();
         audio.AddNode<MusicManager>();
