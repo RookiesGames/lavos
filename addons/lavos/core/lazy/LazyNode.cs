610908b1ee3eae4751c4625f22d61189f88e0445
Duarte Roso <Duarte.Roso@xa.epicgames.com>
2024-09-15 12:14:20 +0200

    Update LazyNode.cs

--- addons/lavos/core/lazy/LazyNode.cs
+++ addons/lavos/core/lazy/LazyNode.cs
@@ -7,8 +7,34 @@ public struct LazyNode<T> where T : Node
 {
     public LazyNode() : this(OmniNode.Instance) { }
     public LazyNode(Node parent) { _parent = parent; }
+    public LazyNode(string name, Node parent = null) : this(parent ?? OmniNode.Instance)
+    {
+        _name = name;
+    }
 
     Node _parent = null;
+    string _name = string.Empty;
     T _instance;
-    public T Node => _instance ??= _parent.GetNodeInTreeByType<T>();
+    /// <summary>
+    /// Node instance
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns>Returns the instance of the Node</returns>
+    public T Node
+    {
+        get
+        {
+            if (_instance != null)
+            {
+                return _instance;
+            }
+            //
+            if (string.IsNullOrEmpty(_name))
+            {
+                return _instance = _parent.GetNodeInTreeByType<T>();
+            }
+            //
+            return _parent.GetNodeInTreeByName<T>(_name);
+        }
+    }
 }
