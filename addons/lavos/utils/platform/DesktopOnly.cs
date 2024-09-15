0ebce2d8065171639ccfac3144e67251d77a012d
Duarte Roso <Duarte.Roso@xa.epicgames.com>
2024-09-15 12:15:07 +0200

    Update DesktopOnly.cs

--- addons/lavos/utils/platform/DesktopOnly.cs
+++ addons/lavos/utils/platform/DesktopOnly.cs
@@ -6,11 +6,13 @@ public sealed partial class DesktopOnly : Node
 {
     public override void _Ready()
     {
-#if GODOT_PC
-        this
-#else
-            GetParent()
-#endif
-            .RemoveSelfDeferred();
+        if (PlatformUtils.IsDebug)
+        {
+            this.RemoveSelfDeferred();
+        }
+        else
+        {
+            GetParent()?.RemoveSelfDeferred();
+        }
     }
 }
