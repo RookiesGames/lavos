d9a02460d50ebc1b49268e0b65e64ec26b94d8a9
Duarte Roso <Duarte.Roso@xa.epicgames.com>
2024-09-15 12:15:09 +0200

    Update MobileOnly.cs

--- addons/lavos/utils/platform/MobileOnly.cs
+++ addons/lavos/utils/platform/MobileOnly.cs
@@ -5,4 +5,15 @@ namespace Lavos.Utils.Platform;
 
 public sealed partial class MobileOnly : Node
 {
+    public override void _Ready()
+    {
+        if (PlatformUtils.IsMobile)
+        {
+            this.RemoveSelfDeferred();
+        }
+        else
+        {
+            GetParent()?.RemoveSelfDeferred();
+        }
+    }
 }
